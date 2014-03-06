602ca28cb4c1a034a113ef6ff629cdaa45f62175
shantanu1227 <shantanu1227@gmail.com>
2014-03-07 00:17:39 +0530

    Added Code Ignitor

---
+++ application/views/home1.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="<?php echo(CSS.'vinfostyle1.css');?>">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+ <link href="<?php echo(CSS.'js-image-slider.css');?>" rel="stylesheet" type="text/css" />
+    <script src="<?php echo(JS.'js-image-slider.js');?>" type="text/javascript"></script>
+<script src="<?php echo (JS.'jquery-1.7.1.min.js');?>" type="text/javascript"></script>
+<title>Vinfocity</title>
+</head>
+<body>
+<div class="wrapper">
+<div class="loginaccount">
+<div class="reglog">Login/Register</div>
+<div class="myaccount">My Account</div>
+</div>
+<div class="header">
+<div id="mono">Vinfocity</div>
+<div class="searchdiv">search here</div>
+</div>
+
+<div class="navigation">
+<div class="navbar">
+<ul>
+<li><a>HOME</a></li>
+<li><a>FOOD</a></li>
+<li><a>PROVISION</a></li>
+<li><a>MEDICAL</a></li>
+<li><a>STATIONARY</a></li>
+<li><a>LAUNDRY</a></li>
+<li><a>PERIPHERALS</a></li>
+<li><a>PRINT/COPY</a></li>
+<li><a>ABOUT US</a></li>
+<li><a>FAQS</a></li>
+</ul>
+</div>
+
+</div>
+<div class="offers">
+<div class="imgslide">
+<div id="slider">
+<img src="<?php echo(IMG.'infocity1.jpg');?>"></img>
+<img src="<?php echo(IMG.'infocity2.jpg');?>"></img>
+<img src="<?php echo(IMG.'infocity3.jpg');?>"></img>
+
+</div>
+</div>
+</div>
+<div class="trending">
+<div class="trendobject">
+<img src="<?php echo(IMG.'trend1.jpg');?>"></img>
+<div class="trendtext">
+<div class="offdes">Buy two classmate notebooks and get one free</div>
+<div class="trendshop"> 
+<div class="trendshophead">SHOP NAME: </div>
+<div class="trendshopname"> Oxford dictionary </div>
+</div>
+
+</div>
+</div>
+<div class="trendobject">
+<img src="<?php echo(IMG.'trend2.jpg');?>"></img>
+<div class="trendtext">
+<div class="offdes">Get one allu patty and a coke for rs.100</div>
+<div class="trendshop"> 
+<div class="trendshophead">SHOP NAME: </div>
+<div class="trendshopname"> Subway </div>
+</div>
+</div>
+</div>
+<div class="trendobject">
+<img src="<?php echo(IMG.'trend3.jpg');?>"></img>
+<div class="trendtext">
+<div class="offdes">Get 7kg laundry service with two 11kg laundry service free</div>
+<div class="trendshop"> 
+<div class="trendshophead">SHOP NAME: </div>
+<div class="trendshopname"> Laundromart </div>
+</div>
+</div>
+</div>
+<div class="trendobject">
+<img src="<?php echo(IMG.'trend1.jpg');?>"></img>
+<div class="trendtext">
+<div class="offdes">Buy two classmate notebooks and get one free</div>
+<div class="trendshop"> 
+<div class="trendshophead">SHOP NAME: </div>
+<div class="trendshopname"> Oxford dictionary </div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+<div class="todaymenu">
+<div class="todaymenuhead">TODAY'S MENU</div>
+<div class="restdetail">
+<div class="resthead">MATARANI</div>
+<div class="restmenu">Aalo fry, chole, daal, chawal, bhindi, kadi</div>
+</div>
+<div class="restdetail">
+<div class="resthead">NAGPOOJA</div>
+<div class="restmenu">Chana masala, aalo gobhi, daal, chawal, raayta, bengan</div>
+</div>
+<div class="restdetail">
+<div class="resthead">USP</div>
+<div class="restmenu">garlic bread, margherita, italiano, panini, thin crust</div>
+</div>
+</div>
+</div>
+<div class="feedback">FEEDBACK</div>
+<div class="footer">
+
+</div>
+</body>
+</html>
