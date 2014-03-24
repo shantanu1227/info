97863ba41b74517d1e1f731fec307fd461e1a4d6
shantanu1227 <shantanu1227@gmail.com>
2014-03-25 01:38:25 +0530

    Removed unwanted files

--- application/config/upload.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-		$config['upload_path'] = base_url()."assets/img"."/kavya"."/";
-		$config['allowed_types'] = 'jpg|png';
-		$config['max_size']	= '50000';
-		$config['max_width']  = '1024';
-		$config['max_height']  = '768';
