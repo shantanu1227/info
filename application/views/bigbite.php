830d0fcf0bb30547e5ea4c5ec32df849d390a993
riagupta <ria.pgupta9@gmail.com>
2014-03-28 14:29:48 +0530

    reglog added

--- application/views/bigbite.php
+++ application/views/bigbite.php
@@ -2,11 +2,117 @@
 <html>
     <head>
         <title>Big Bite</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo(CSS.'reglogcss.css');?>">
         <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
         <link rel='stylesheet' type="text/css" href="<?php echo(CSS . 'bigbitestyle.css'); ?>">
         <link rel="stylesheet" type="text/css" href="<?php echo(CSS . 'js-image-slider.css'); ?>">
         <script type='text/javascript' src="<?php echo(JS . 'js-image-slider.js'); ?>"></script>
         <script type='text/javascript' src="<?php echo(JS . 'jquery-1.7.1.min.js'); ?>"></script>
+	  <script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
+          <!-- jQuery Form Validation code -->
+	  <script>
+	  
+	  // When the browser is ready...
+	  $(function() {
+	  
+		// Setup form validation on the #register-form element
+		$.validator.addMethod(
+			"regex",
+			function(value, element, regexp) {
+				var re = new RegExp(regexp);
+				return re.test(value);
+			},
+			"Please check your input."
+		);
+
+		$("#registerform").validate({
+			errorElement: "div",
+			// Specify the validation rules
+			rules: {
+				fullname: "required",
+				username: {
+					required: true,
+					number: true,
+					range: [201001001, 201499999]
+				},
+				password: {
+					required: true,
+					minlength: 5
+				},
+				roomno: {
+					required: true,
+					regex: '^[A-H]{1}-[1-3]{1}[0-2]{1}[0-9]{1}$'
+					},
+				mobileno: {
+					required: true,
+					minlength: 10
+				},
+			},
+			
+			// Specify the validation error messages
+			messages: {
+				fullname: "",
+				username: {
+					required:'',
+					maxlength: "Please enter a your DA-IICT ID.",
+					minlength: "Please enter a your DA-IICT ID.",
+					range: "Please enter a your DA-IICT ID."
+				},
+				password: {
+					required:'',
+					minlength: "Password must be at least 5 characters long."
+				},
+				roomno: {
+					required:'',
+					regex: "Please enter a valid roomno."
+					},
+				mobileno: {
+					required:'',
+					minlength: "Please enter a valid mobile number."
+					},
+			},
+			
+			submitHandler: function(form) {
+				form.submit();
+			}
+		});
+		$("#loginform").validate({
+			errorElement: "div",
+			// Specify the validation rules
+			rules: {
+				username: {
+					required: true,
+					number: true,
+					range: [201001001, 201499999]
+				},
+				password: {
+					required: true,
+					minlength: 5
+				}
+			},
+			
+			// Specify the validation error messages
+			messages: {
+				username: {
+					required:'',
+					maxlength: "Please enter a your DA-IICT ID.",
+					minlength: "Please enter a your DA-IICT ID.",
+					range: "Please enter a your DA-IICT ID."
+				},
+				password: {
+					required:'',
+					minlength: "Password must be at least 5 characters long."
+				}
+			},
+			
+			submitHandler: function(form) {
+				form.submit();
+			}
+		});
+
+	  });
+	  
+	  </script>
     </head>
     <body>
         <div id="box">
@@ -465,5 +571,6 @@
         </div>
         
 		<div id="feedback">Feedback</div>
+                <?php include 'reglog.php'; ?>
     </body>
 </html>
