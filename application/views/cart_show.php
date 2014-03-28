96782011139d36611c277469157f2d5d3c45d2e4
parthdhoot <parthdhoot@gmail.com>
2014-03-29 04:51:56 +0530

    changes

--- application/views/cart_show.php
+++ application/views/cart_show.php
@@ -1,19 +1,36 @@
+
 <?php if(!$this->cart->contents()){
     echo 'You don\'t have any items yet.';
 }else{
 ?>
- 
+<html>
+	<head>
+		<title>My Cart</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo(CSS.'vinfostyle.css');?>">
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<link href="<?php echo(CSS.'js-image-slider.css');?>" rel="stylesheet" type="text/css" />
+		<link href="<?php echo(CSS.'reglogcss.css');?>" rel="stylesheet" type="text/css" />
+		<!--script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script-->
+		<script src="<?php echo(JS.'js-image-slider.js');?>" type="text/javascript"></script>
+		<script src="<?php echo (JS.'jquery-1.7.1.min.js');?>" type="text/javascript"></script>
+		<script src="<?php echo (JS.'core.js');?>" type="text/javascript"></script>
+	</head>
+	
+	<body>
+		
+	<div id="box">
+<?php include 'header1.php'; ?>	
 <?php echo form_open('cart/updatecart'); ?>
-<table width="100%" cellpadding="0" cellspacing="0">
-    <thead>
-        <tr>
-            <td>Qty</td>
-            <td>Item Description</td>
-            <td>Item Price</td>
-            <td>Sub-Total</td>
+<table class="carttable" border="1px" width="100%" cellpadding="0" cellspacing="0">
+    
+        <tr >
+            <th >Qty</td>
+            <th >Item Description</td>
+            <th >Item Price</td>
+            <th >Sub-Total</td>
         </tr>
-    </thead>
-    <tbody>
+    
+    
         <?php $i = 1; ?>
         <?php foreach($this->cart->contents() as $items) { ?>
          
@@ -33,14 +50,17 @@
         <?php } ?>
          
         <tr>
-            <td</td>
+            <td></td>
             <td></td>
             <td><strong>Total</strong></td>
             <td>Rs <?php echo $this->cart->format_number($this->cart->total()); ?></td>
         </tr>
-    </tbody>
+    
 </table>
  
-<p><?php echo form_submit('cart/updatecart', 'Update your Cart'); echo anchor('cart/emptycart', 'Empty Cart');?></p>
-<p><small>If the quantity is set to zero, the item will be removed from the cart.</small></p>
+<p class="updateempty"> <div class="updatecartbut"><?php echo form_submit('cart/updatecart', 'Update your Cart');?></div> <div class="emptycartbut"> <?php echo anchor('cart/emptycart', 'Empty Cart');?> </div> </p>
+
 <?php } ?>
+
+</div>
+</body>
--- application/views/kavya.php
+++ application/views/kavya.php
@@ -63,38 +63,7 @@
 			</div>
 		</div>
 		<div class="menuhead"><i>Products</i></div>
-		<div class="shopproducts">
-		<?php
-		foreach ($output as $product) {
-		?>
-
-			<div class="shopproductitem">
-				<img width="100%" src="<?php echo(IMG . $product->productImage); ?>"></img>
-				<div class="itemname">
-					<?php echo $product->productName; ?>
-				</div>
-				<div class="itemprice">
-					<?php echo "Rs.".$product->price; ?>
-				</div>
-				<?php
-		echo form_open('cart/addtocart'); ?>
-		<label> Quantity </label>
-		 <?php
-		 echo form_input('quantity', '1', 'maxlength="2"');
-		  echo form_hidden('product_id', $product->productId);
-		  echo form_hidden('product_name', $product->productName);
-		  echo form_hidden('product_price', $product->price);
-		    ?> <div class="reloadonadd"> <?php echo form_submit('add', 'Add'); ?> </div> <?php
-				
-		  echo form_close(); ?>
-			</div>
-		<?php
-		}
-		?>	
-			
-		</div>
-	</div>
-	<div id="feedback">Feedback</div>
+		
 	<?php include 'reglog.php'; ?>
 	</body>
 </html>
