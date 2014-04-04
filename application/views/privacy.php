9fd412d867b32bc02194d834e325937409a6f76e
Sahil <201101085daiict@gmail.com>
2014-04-04 15:45:55 +0530

    privacy and terms and conditions added!

---
+++ application/views/privacy.php
@@ -0,0 +1,58 @@
+<!--
+        Document: aboutus
+        Purpose: Page containing information related to Virtual Infocity 
+    -->
+    <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+    <html>
+    <head>
+        <title>Privacy Policy</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <link rel='stylesheet' type="text/css" href="<?php echo(CSS . 'privacystyle.css'); ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo(CSS.'reglogcss.css');?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo(CSS.'commonstyle.css');?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo(CSS.'feedback.css');?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo(CSS . 'js-image-slider.css'); ?>">
+        <script type='text/javascript' src="<?php echo(JS . 'js-image-slider.js'); ?>"></script>
+        <script type='text/javascript' src="<?php echo(JS . 'jquery-1.7.1.min.js'); ?>"></script>
+</head>
+<body>
+    <div id="box">
+        <?php include 'header1.php'; ?>
+        <div class="shopheading">
+            privacy
+        </div>
+
+       <div class="about">
+       
+       1. GENERAL PRINCIPLES
+        <p>Protecting your privacy is very important to us. We have accordingly developed this Privacy Policy to protect your personal information and keep it confidential.Our privacy policy is subject to change at any time without notice. 
+        To make sure you are aware of any changes, please review this policy periodically.
+        We categorise information about you (collectively referred to as "Personal Information") as follows:</p>    
+
+            <p>
+            (a) Profiling Information: Information which you provide when you register for a Service, which may include some or all of the following: - 
+            Information about your personal identity such as gender, marital status, age, preferences,etc.; - Your financial information such as your banking details and any information relating to your income and lifestyle levels; 
+            and - Your contact details such as your physical addresses, postal addresses, telephone and fax numbers and the like. . 
+            </p>
+            <p>
+            (b) Payment and Account Information: Your account history with us including (without limitation) all billing information and communications, payment history etc. (c) Log information:Information such as your web request, IP address, browser type, browser language, date and time of request.(d) User IDs: Your usernames, passwords, email addresses and other security-related information used by you in relation to our Services. (e) Stored Information: Data either created by you or by a third party and which you wish to store on our servers such as image files, documents etc. We only collect your Personal Information to conduct our business and to enable us to deliver and improve our Services. 
+            We do not for any reason whatsoever sell your Personal Information to any third party or otherwise trade on it.
+            </p>
+            <p>
+            (c) We will only disclose your Personal Information in accordance with this Privacy Policy. If we want to use it for any other purpose, we will obtain your prior written consent. If you decline to submit personal information to us, then we will unfortunately not be in a position to provide the Services to you. Any of your information which you provide when you use certain Services and are to an open, public environment or forum including (without limitation) any blog, community or discussion board, is not confidential, does not constitute Personal Information, and is not subject to protection under Privacy Policy. Since such public environments are accessible by third parties, 
+            it is possible that third parties may collect and collate and use such information for their own purposes. You should accordingly be careful when deciding to share any of your Personal Information in such public environments. Information, which is disclosed publicly, is also shared with our affiliates, third party service providers, sponsors of competitions etc. unless expressly stated otherwise. We are not liable to you or any third party for any damages that you or any third party may suffer howsoever arising from your disclosure of Personal Information in any public environment. You accordingly disclose information in a public environment at your own risk.
+            </p>
+            <p>
+        2. RIGHT TO COLLECT
+        By accepting the Terms of Service you agree that we may collect and store your Personal Information as long as you use our Services subject to the limitations set out in this Privacy Policy.
+
+        We collect your Profiling and Account Information for the following reasons:We need your identity details, contact details, and account history to manage our relationship with you and provide Services to you. We may use this information for suggesting products and promotional offers and schemes.
+        </p>
+        </div>
+    
+    </div>
+    <div id="feedback">Feedback</div>
+    <?php include 'feedback.php'; ?>
+    <?php include 'reglog.php'; ?>
+</body>
+</html>
